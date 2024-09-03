6ca54a2c52246b1dd109259513ac11856f0cf4e0
maruf-dev <maruf123erkinov@gmail.com>
2024-09-04 02:01:17 +0500

    hero 50%

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,20 +1,27 @@
-import ArrowIcon from '@/assets/arrow-right.svg'
-
-
+import ArrowIcon from '@/assets/arrow-right.svg';
+import cogImage from '@/assets/cog.png';
+import Image from 'next/image';
+import cylinderImage from '@/assets/cylinder.png';
 export const Hero = () => {
   return (
-    <section>
+    <section className='pt-8 pb-20 md:pt-5 md:pb-10 bg-[radial-gradient(ellipse_200%_100%_at_bottom_left,#183EC2,#EAEEFE_100%)] overflow-x-clip'>
       <div className="container">
-        <div className="">
-          <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here </div>
-          <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Pathway to productivity</h1>
-          <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
-          <div className="flex gap-1 items-center mt-[30px]">
-            <button className="btn btn-primary">Get for free</button>
-            <button className="btn btn-text gap-1">
-              <span>Learn more</span>
-              <ArrowIcon className='h-5 w-5' />
-            </button>
+        <div className="md:flex items-center">
+          <div className='md:w-[478px]'>
+            <div className="text-sm inline-flex border border-[#222]/10 px-3 py-1 rounded-lg tracking-tight">Version 2.0 is here </div>
+            <h1 className="text-5xl md:text-7xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Pathway to productivity</h1>
+            <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
+            <div className="flex gap-1 items-center mt-[30px]">
+              <button className="btn btn-primary">Get for free</button>
+              <button className="btn btn-text gap-1">
+                <span>Learn more</span>
+                <ArrowIcon className='h-5 w-5' />
+              </button>
+            </div>
+          </div>
+          <div className="mt-20 md:mt-0 md:h-[648px] md:flex-1 md:relative">
+            <Image src={cogImage} alt='Cog image' className='md:absolute md:h-full md:w-auto md:max-w-none md:-left-6'/>
+            <Image src={cylinderImage} alt='cylinder' width={220} height={220} className='hidden md:block -top-8 -left-32 md:absolute'/>
           </div>
         </div>
       </div>
