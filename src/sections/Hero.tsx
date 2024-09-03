dde612751f9b057692e12e15a6993a0758dc9b55
maruf-dev <maruf123erkinov@gmail.com>
2024-09-04 01:07:11 +0500

    change in hero arrow

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,6 @@
+import ArrowIcon from '@/assets/arrow-right.svg'
+
+
 export const Hero = () => {
   return (
     <section>
@@ -7,8 +10,11 @@ export const Hero = () => {
           <h1 className="text-5xl font-bold tracking-tighter bg-gradient-to-b from-black to-[#001E80] text-transparent bg-clip-text mt-6">Pathway to productivity</h1>
           <p className="text-xl text-[#010D3E] tracking-tight mt-6">Celebrate the joy of accomplishment with an app designed to track your progress, motivate your efforts, and celebrate your successes.</p>
           <div className="flex gap-1 items-center mt-[30px]">
-            <button className="">Get for free</button>
-            <button>Learn more</button>
+            <button className="btn btn-primary">Get for free</button>
+            <button className="btn btn-text gap-1">
+              <span>Learn more</span>
+              <ArrowIcon className='h-5 w-5' />
+            </button>
           </div>
         </div>
       </div>
