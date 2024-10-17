76e5ae910cc923302a84c4c69d747675a29f7387
maruf-dev <maruf123erkinov@gmail.com>
2024-10-17 22:58:49 +0500

    add animation productshowcase section

--- src/sections/ProductShowcase.tsx
+++ src/sections/ProductShowcase.tsx
@@ -1,13 +1,21 @@
+"use client";
 import productImage from '@/assets/product-image.png';
 import pyramidImage from '@/assets/pyramid.png';
 import tubeImage from '@/assets/tube.png';
 import Image from 'next/image';
-
+import { motion, useScroll, useTransform } from "framer-motion";
+import { useRef } from  'react';
 
 export const ProductShowcase = () => {
+  const sectionRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: sectionRef,
+    offset: ['start end', 'end start']
+  });
+  const translateY = useTransform(scrollYProgress, [0, 1], [150, -150])
   return (
     <>
-      <section className='bg-gradient-to-b from-[#fffff] to-[#d2dcff] py-24 overflow-x-clip'>
+      <section ref={sectionRef} className='bg-gradient-to-b from-[#fffff] to-[#d2dcff] py-24 overflow-x-clip'>
         <div className="container">
           <div className="section-heading">
             <div className="flex justify-center">
@@ -19,8 +27,26 @@ export const ProductShowcase = () => {
 
             <div className="relative">
               <Image src={productImage} alt='Product Image' className='mt-10'/>
-              <Image src={pyramidImage} alt='Pyramid Image' height={262} width={262} className='hidden md:block absolute -right-36 -top-32' />
-              <Image src={tubeImage} alt='Tube Image' height={248} className='hidden md:block absolute bottom-24 -left-36'  />
+              <motion.img
+                src={pyramidImage.src}
+                alt='Pyramid Image'
+                height={262}
+                width={262}
+                className='hidden md:block absolute -right-36 -top-32'
+                style={{
+                  translateY,
+                }}
+                />
+              <motion.img
+                src={tubeImage.src}
+                alt='Tube Image'
+                height={248}
+                width={248}
+                className='hidden md:block absolute bottom-24 -left-36'
+                style={{
+                  translateY,
+                }}
+                />
             </div>
         </div>
       </section>
